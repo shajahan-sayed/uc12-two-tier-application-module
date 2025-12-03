3b82eb7db78b9870a8e4244f6a2e5bba9cdd374c
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-03 15:38:35 +0530

    Create variable.tf

---
+++ modules/subnets/variable.tf
@@ -0,0 +1,20 @@
+variable "public1_cidr" {
+  type = string
+}
+variable "public2_cidr" {
+  type = string
+}
+variable "private1_cidr" {
+  type = string
+}
+variable "private2_cidr" {
+  type = string
+}
+variable "availability_zone1" {
+  type = string
+}
+variable "availability_zone2" {
+  type = string
+}
+
+
