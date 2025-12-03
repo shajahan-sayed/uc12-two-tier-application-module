0153b2d08b54365eb74f82a592533cb63b64a5e7
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-03 17:53:36 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "ec2_sg1" {
+   vpc_id = aws_vpc.vpc_module.id
+
+   ingress {
+     description = "allow http port"
+     from_port = 80
+     to_port = 80
+     protocol = "tcp"
+     security_groups = [aws_security_group.alb-sg1.id]
+    }
+    ingress { 
+      description = "allowing ssh port" 
+      from_port = 22
+      to_port  = 22
+      protocol = "tcp"
+    }
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "ec2_sg1"
+    }
+  }
+
+resource "aws_instance" "ec2_alb" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.ec2_sg1.id]
+    
+    tags = {
+      Name = "ec2_alb"
+    }
+  }
