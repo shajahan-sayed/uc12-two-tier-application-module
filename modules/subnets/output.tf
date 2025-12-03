8c8dea606ad8bb6803f3427daad2413c09b045b1
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-03 15:45:05 +0530

    Create output.tf

---
+++ modules/subnets/output.tf
@@ -0,0 +1,12 @@
+output "public1_subnet_id" {
+  value = aws_subnet.public1.id
+}
+output "public2_subnet_id" {
+  value = aws_subnet.public2.id
+}
+output "private1_subnet_id" {
+  value = aws_subnet.private1.id
+}
+output "private2_subnet_id" {
+  value = aws_subnet.private1.id
+}
